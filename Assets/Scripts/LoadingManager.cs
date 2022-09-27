4a94325f34abcf41d7e6f71cdd20dddca6218601
timothe.rosaz <timothe.rosaz@insa-rennes.fr>
2022-09-27 11:58:42 +0200

    Added the basic structure of scenes. Added a way to load the play scene.

---
+++ Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadingManager : MonoBehaviour {
+	public static LoadingManager Instance { private set; get; }
+
+	[SerializeField] private int stage = 0;
+	[SerializeField] private string levelSceneName = "LevelScene";
+	[SerializeField] private GameObject testPrefab;
+
+	private void Start() {
+		if(Instance != null) {
+			Debug.LogError("Cannot have multiple LoadingManager. Removing this one.");
+			Destroy(this);
+			return;
+		}
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void Reset() {
+		stage = 0;		
+	}
+
+	public void NextStage() {
+		stage++;
+		Debug.Log("Preparing stage " + stage + ".");
+
+		Debug.Log("Creating procedural generator...");
+		//TODO ProceduralGenerator generator = new ProceduralGenerator();
+
+		Debug.Log("Creating the level layout...");
+		//TODO Texture2D layout  = generator.GenerateTexture(w, h);
+
+		Debug.Log("Creating scene...");
+		StartCoroutine(LoadAsyncScene());
+	}
+
+	private IEnumerator LoadAsyncScene(/*Texture2D layout*/) {
+		// The Application loads the Scene in the background at the same time as the current Scene.
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelSceneName, LoadSceneMode.Additive);
+
+		// Wait until the last operation fully loads to return anything
+		while(!asyncLoad.isDone) {
+			yield return null;
+		}
+
+		// TODO : IMPLEMENT LAYOUT !
+		// TODO : create ENEMIES !
+
+		var test = Instantiate(testPrefab);
+		SceneManager.MoveGameObjectToScene(test, SceneManager.GetSceneByName(levelSceneName));
+
+		// Unload the previous Scene
+		SceneManager.UnloadSceneAsync("LoadingScreen");
+	}
+
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Objectif : savoir si le joueur peut accéder aux "secrets" et "shop"
+
+	[SerializeField] private Button shopButton;
+
+	private void Start() {
+		var runs = PersistentData.RunsAmount;
+		if(runs > 0)
+			shopButton.gameObject.SetActive(true);
+	}
+
+	public void SelectedQuitApplication() {
+		Debug.Log("Quitting application. See you a next time !");
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit(0);
+#endif
+	}
+
+	public void SelectedStartGame() {
+		SceneManager.LoadScene("LoadingScreen");
+		LoadingManager.Instance.NextStage();
+	}
+
+	public void SelectedShop() {
+		Debug.Log("shop selected.");
+	}
+	
+}
