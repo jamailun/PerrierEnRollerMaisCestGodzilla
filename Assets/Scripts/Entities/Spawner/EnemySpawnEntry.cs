034dcba1b887e5952676d4a4754ce5a3b4046cff
timothe.rosaz <timothe.rosaz@insa-rennes.fr>
2022-10-04 23:59:15 +0900

    Started the spawner system structure

---
+++ Assets/Scripts/Entities/Spawner/EnemySpawnEntry.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public struct EnemySpawnEntry {
+
+	public Enemy enemyPrefab;
+
+	public float minX, maxX;
+
+	public float additionalRadius;
+
+	public float cooldown;
+	
+}
---
+++ Assets/Scripts/Entities/Spawner/LevelEnemiesSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelEnemySpawner", menuName = "PERMCG/Level Enemy Spawner", order = 8)]
+public class LevelEnemiesSpawner : ScriptableObject {
+
+	public EnemySpawnEntry[] enemies;
+
+	public float minWaveCooldown;
+
+	public float maxWaveCooldown;
+
+	public float NextCooldown() {
+		return Random.Range(minWaveCooldown, maxWaveCooldown);
+	}
+
+}
